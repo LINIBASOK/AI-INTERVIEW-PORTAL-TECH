a5affdc4302fb30ae358c140878a691e206a8250
lini-git <liniasok7@gmail.com>
2025-10-23 17:01:36 +0530

    fix for path

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Header from './components/Layout/Header';
